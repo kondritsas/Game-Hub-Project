92a86271361af2582028a583a53e1afff7e35d65
kondritsas <154312323+kondritsas@users.noreply.github.com>
2024-04-17 12:02:01 +0300

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,19 +7,19 @@ import GameCardContainer from './GameCardContainer';
 
 
 const GameGrid = () => {
-  const{games,error, isLoading} = useGames();
+  const{data,error, isLoading} = useGames();
   const skeletons = [1,2,3,4,5,6];
 
   return (
     <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm: 1, md:2, lg:3, xl:5}} padding="10px"spacing={10}>
+        <SimpleGrid columns={{sm: 1, md:2, lg:3, xl:5}} padding="10px"spacing={3}>
           {isLoading && skeletons.map((skeleton)=> (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton}></GameCardSkeleton>
             </GameCardContainer>
           ))}
-            {games.map((game) => (
+            {data.map((game) => (
               <GameCardContainer>
                 <GameCard key={game.id} game={game}></GameCard>
               </GameCardContainer>))}
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import useGenres from '../hooks/useGenres';
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import useGenres, { Genre } from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-urls';
 
 const GenreList = () => {
-  const {genres} = useGenres();
+  const {data} = useGenres();
   return (
-    <ul>
-        {genres.map((genre)=> <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+        {data.map((genre)=> 
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}></Image>
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>)}
+    </List>
   )
 }
 
 export default GenreList
+
+
